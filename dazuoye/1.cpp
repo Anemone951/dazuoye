709814d8fa2274b046b3c5825336ea6a12b01b0b
Anemone951 <166927913+Anemone951@users.noreply.github.com>
2024-08-01 16:27:04 +0800

    Add files via upload

---
+++ dazuoye/1.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <algorithm>
+
+using namespace std;
+
+string randomword(int length) {
+    string word;
+    static const char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
+    for (int i = 0; i < length; i++) {
+        word += alphabet[rand() % (sizeof(alphabet) - 1)];
+    }
+    return word;
+}
+
+string randomline() {
+    int num_words = rand() % 11 + 5;
+    string line;
+    for (int i = 0; i < num_words; i++) {
+        if (i > 0) line += " ";
+        line += randomword(rand() % 17 + 4);
+    }
+    return line;
+}
+
+void file(const string& filename, int num_lines) {
+    ofstream file;
+    file.open(filename, ios::out);
+    if (!file.is_open()) {
+        cerr << "Unable to open file: " << filename << endl;
+        return;
+    }
+    cout << "Writing " << num_lines << " lines to " << filename << endl;
+    for (int i = 0; i < num_lines; i++) {
+        file << randomline() << "\n";
+    }
+    file.close();
+}
+
+int main() {
+    srand(static_cast<unsigned int>(time(0)));
+
+    unsigned int num_files = 20;
+    int min_lines = 500000;
+    int max_lines = 4000000;
+    vector<int> files;
+
+    while (files.size() < num_files) {
+        int num_lines = rand() % (max_lines - min_lines + 1) + min_lines;
+        files.push_back(num_lines);
+    }
+
+    int current_min = *min_element(files.begin(), files.end());
+    int current_max = *max_element(files.begin(), files.end());
+
+    if (current_max - current_min < 2000000) {
+        for (int& num_lines : files) {
+            if (current_max - current_min >= 2000000) break;
+            if (num_lines == current_max) continue;
+            num_lines = max_lines;
+            current_max = max_lines;
+        }
+       
+        for (int& num_lines : files) {
+            if (current_max - current_min >= 2000000) break;
+            if (num_lines == current_min) continue;
+            num_lines = min_lines;
+            current_min = min_lines;
+        }
+    }
+
+    for (int i = 0; i < num_files; i++) {
+        string filename = "random_file_" + to_string(i + 1) + ".txt";
+        file(filename, files[i]);
+        cout << "Generated " << filename << " with " << files[i] << " lines." << endl;
+    }
+    return 0;
+}
